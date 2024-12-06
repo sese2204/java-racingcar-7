223125b20593445856e121ddb3178e284ea14b72
sese2204 <sese2204@naver.com>
2024-12-06 23:07:09 +0900

    feat(Car): 자동차 정보와 전진 기능 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+public class Car {
+    private String carName;
+    private int position;
+
+    public Car(String carName, int position) {
+        this.carName = carName;
+        this.position = position;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void goFoward() {
+        position++;
+    }
+}
